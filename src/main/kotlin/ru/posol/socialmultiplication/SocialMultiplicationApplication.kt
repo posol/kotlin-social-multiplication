ba7e98443d90735184e2b1b954197629ab569e15
POSOL-ПК\posol <posol2008@gmail.com>
2018-04-19 22:44:54 +0500

    generate app

---
+++ src/main/kotlin/ru/posol/socialmultiplication/SocialMultiplicationApplication.kt
@@ -0,0 +1,11 @@
+package ru.posol.socialmultiplication
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SocialMultiplicationApplication
+
+fun main(args: Array<String>) {
+    runApplication<SocialMultiplicationApplication>(*args)
+}
