afc4a6a55703a25acd6816058db53a2490f2b3b2
POSOL-ПК\posol <posol2008@gmail.com>
2018-05-24 23:37:44 +0500

    add  cross in services

--- social-multiplication-service/src/main/kotlin/ru/posol/microservice/multiplication/configuration/WebConfiguration.kt
+++ social-multiplication-service/src/main/kotlin/ru/posol/microservice/multiplication/configuration/WebConfiguration.kt
@@ -1,10 +1,17 @@
 package ru.posol.microservice.multiplication.configuration
 
+import org.springframework.context.annotation.Configuration
 import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.EnableWebMvc
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 
+@Configuration
+@EnableWebMvc
 class WebConfiguration : WebMvcConfigurerAdapter() {
 
+    /**
+     * Enables Cross-Origin Resource Sharing (CORS)
+     */
     override fun addCorsMappings(registry: CorsRegistry) {
         registry.addMapping("/**")
     }
