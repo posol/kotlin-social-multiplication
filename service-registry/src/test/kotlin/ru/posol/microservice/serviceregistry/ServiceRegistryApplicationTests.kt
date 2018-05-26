e90c917f533ea2f7012c6e7359d36f5f89c88572
POSOL-ПК\posol <posol2008@gmail.com>
2018-05-26 21:43:42 +0500

    add service-registry

---
+++ service-registry/src/test/kotlin/ru/posol/microservice/serviceregistry/ServiceRegistryApplicationTests.kt
@@ -0,0 +1,16 @@
+package ru.posol.microservice.serviceregistry
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
