973b8c9b8004f7aa89617cf763056f5ab7ab91ed
POSOL-ПК\posol <posol2008@gmail.com>
2018-06-06 00:04:43 +0500

    add logs in rest api

--- social-multiplication-service/src/main/kotlin/ru/posol/microservice/multiplication/controller/MultiplicationController.kt
+++ social-multiplication-service/src/main/kotlin/ru/posol/microservice/multiplication/controller/MultiplicationController.kt
@@ -1,6 +1,8 @@
 package ru.posol.microservice.multiplication.controller
 
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -13,9 +15,17 @@ application.
  */
 @RestController
 @RequestMapping("/multiplications")
-class MultiplicationController(@Autowired val multiplicationService: MultiplicationService) {
+class MultiplicationController(
+        @Autowired val multiplicationService: MultiplicationService,
+        @Value("\${server.port}") val serverPort: Int
+) {
+
+    private val log = LoggerFactory.getLogger(MultiplicationController::class.java)
 
     @GetMapping("/random")
-    fun getRandomMultiplication(): Multiplication = multiplicationService.createRandomMultiplication()
+    fun getRandomMultiplication(): Multiplication {
+        log.info("Generating a random multiplication from server $serverPort")
+        return multiplicationService.createRandomMultiplication()
+    }
 
 }
--- social-multiplication-service/src/main/kotlin/ru/posol/microservice/multiplication/controller/MultiplicationResultAttemptController.kt
+++ social-multiplication-service/src/main/kotlin/ru/posol/microservice/multiplication/controller/MultiplicationResultAttemptController.kt
@@ -1,6 +1,8 @@
 package ru.posol.microservice.multiplication.controller
 
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 import ru.posol.microservice.multiplication.domain.MultiplicationResultAttempt
@@ -9,7 +11,12 @@ import ru.posol.microservice.multiplication.service.MultiplicationService
 
 @RestController
 @RequestMapping("/results")
-class MultiplicationResultAttemptController(@Autowired val multiplicationService: MultiplicationService) {
+class MultiplicationResultAttemptController(
+        @Autowired val multiplicationService: MultiplicationService,
+        @Value("\${server.port}")val serverPort: Int
+) {
+
+    private val log = LoggerFactory.getLogger(MultiplicationResultAttemptController::class.java)
 
     @PostMapping
     fun postResult(@RequestBody multiplicationResultAttempt: MultiplicationResultAttempt): ResponseEntity<MultiplicationResultAttempt> {
@@ -25,6 +32,7 @@ class MultiplicationResultAttemptController(@Autowired val multiplicationService
 
     @GetMapping("/{resultId}")
     fun getResultById(@PathVariable("resultId") resultId: Long): ResponseEntity<MultiplicationResultAttempt> {
+        log.info("Retrieving result $resultId from server $serverPort")
         return ResponseEntity.ok(multiplicationService.getResultById(resultId))
     }
 
